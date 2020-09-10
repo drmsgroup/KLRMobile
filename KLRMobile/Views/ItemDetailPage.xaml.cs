edf4cb72eb8019444eee193278695ba7787b3bb5
Eddie Dillon <eddie.d.2000@gmail.com>
2020-09-10 17:43:44 -0500

    Add project files.

---
+++ KLRMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using KLRMobile.ViewModels;
+
+namespace KLRMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
