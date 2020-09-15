fcbadec9c80ee90060cc51c5d7784e759f1b37a3
Eddie Dillon <eddie.d.2000@gmail.com>
2020-09-15 18:27:08 -0500

    Adding in the intial models to support the data

---
+++ KLRMobile/Models/Image.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace KLRMobile.Models
+{
+    public class Image
+    {
+        public string Id { get; set; }
+        public string base64String { get; set; }
+        public int InstrumentId { get; set; }
+    }
+}
---
+++ KLRMobile/Models/Instrument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KLRMobile.Models
+{
+    public class Instrument
+    {
+        public int InstrumentId { get; set; }
+        public string InstrumentType { get; set; }
+        public DateTime DateFiled { get; set; }
+        public string BookName { get; set; }
+        public string BookNumber { get; set; }
+        public string PageNumber { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string UpdatedBy { get; set; }
+        public string Description { get; set; }
+    }
+}
