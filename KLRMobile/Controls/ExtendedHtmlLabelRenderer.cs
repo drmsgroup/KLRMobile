856898c45e6212bd0ae44417c1d24f12f4a61394
Eddie Dillon <eddie.d.2000@gmail.com>
2020-09-18 16:49:13 -0500

    -Added the Title Lien Search UI -Added Custom Date Picker with overridden PlaceHolder

---
+++ KLRMobile/Controls/ExtendedHtmlLabelRenderer.cs
@@ -0,0 +1,23 @@
+﻿using KLRMobile.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedDatePicker), typeof(ExtendedDatePickerRenderer))]
+namespace KLRMobile.Controls
+{
+    public class ExtendedDatePickerRenderer : DatePickerRenderer
+    {
+        public ExtendedDatePicker ExtendedDatePickerElement => Element as ExtendedDatePicker;
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Text = ExtendedDatePickerElement.PlaceHolder;
+            }
+        }
+    }
+}
