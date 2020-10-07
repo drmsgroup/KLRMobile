7384162c6c05a2449a5249b948d4bf8f782333dc
Eddie Dillon <eddie.d.2000@gmail.com>
2020-10-06 19:31:28 -0500

    Adding the Settings Page to the flyout menu

--- KLRMobile/AppShell.xaml.cs
+++ KLRMobile/AppShell.xaml.cs
@@ -13,12 +13,18 @@ namespace KLRMobile
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
             Routing.RegisterRoute(nameof(LaunchScreen), typeof(LaunchScreen));
+            Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
             // Navigation.PushModalAsync(new LoginPage());
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        private void OnSettingsPageClicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new SettingsPage());
         }
     }
 }
