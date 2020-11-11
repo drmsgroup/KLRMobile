f7aabf972528ac672d2cfadbc724814bc5fe3eef
Eddie Dillon <eddie.d.2000@gmail.com>
2020-11-10 18:03:22 -0600

    Adding in the libraries for the results pages and stubbing in the example from SyncFusion

---
+++ KLRMobile/Views/MyOrdersPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace KLRMobile.Views
+{
+    /// <summary>
+    /// Page to show the my order list. 
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyOrdersPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyOrdersPage" /> class.
+        /// </summary>
+        public MyOrdersPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
